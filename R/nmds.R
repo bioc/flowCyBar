7a6e229f77a9ae2319448905292f23f3fda4498d
Joachim Schumann <joachim.schumann@ufz.de>
2014-12-08 08:01:58 +0000

    added support for class matrix
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCyBar@97474 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/nmds.R
+++ R/nmds.R
@@ -31,3 +31,34 @@ definition = function(x,distance="bray",autotransform=FALSE,zerodist="add",group
   return(newlist)
   
 })
+
+
+setMethod("nmds", signature=(x="matrix"),
+          definition = function(x,distance="bray",autotransform=FALSE,zerodist="add",group,main="",type="p",cex=0.6,pos=4,shrink=TRUE,legend_pos="topleft",pch=1,col="black",abiotic,p.max=0.05,col_abiotic="magenta",verbose=FALSE,...) {
+            newlist<-list()
+            mds.out <- metaMDS(x, distance=distance, autotransform=autotransform, zerodist=zerodist,...)    # performs nonmetric multidimensional scaling
+            print(rownames(mds.out$points))
+            newlist$mds.out<-mds.out
+            plot(mds.out, type="n",main=main) # plot results
+            text(mds.out,cex=cex,pos=pos,shrink=shrink,...)    # data label as text  
+            
+            if(missing(group))  # check for group file
+              points(mds.out, type=type,pch=pch,col=col,...)  # add points  
+            
+            else{
+              color<-group[,1]
+              points(mds.out,type=type, col=color , pch=as.numeric(color))
+              legend(x=legend_pos, legend=unique(color),  pch=as.numeric(unique(color)), col=as.numeric(unique(color)))
+              ordihull(mds.out, groups=color, lty=2, col="darkgrey")   
+            }
+            
+            if(!missing(abiotic)){  # check for abiotic file
+              ef<-envfit(mds.out, abiotic, permutation=999)     # calculates the relevant environmental parameters for the nMDS #result based on 999 Monte-Carlo permutations
+              plot (ef,cex=cex,p.max=p.max, col=col_abiotic)       # plots the most relevant environmental parameters (with a p.max <=0.05) into the MDS
+              newlist$ef<-ef    
+            }
+            
+            if(verbose)
+              return(newlist)
+            
+          })
--- R/normalize.R
+++ R/normalize.R
@@ -18,3 +18,20 @@ definition = function(x,method="mean",show_avg=TRUE,...) {
   
   return(format(normalized,...))
 })
+
+setMethod("normalize", signature=(x="matrix"),
+          definition = function(x,method="mean",show_avg=TRUE,...) {
+            
+            if(method=="mean"){
+              normalize<-apply(x,1,function(y) y/colMeans(x,na.rm=TRUE))  # normalize the values
+              normalized<-as.data.frame(t(normalize))
+              if (show_avg)
+                print (colMeans(x,na.rm=TRUE),...) # print averages
+            }
+            else if (method=="first"){
+              normalize<-apply(x,1,function(y) y/as.numeric(head(x,n=1L)))  # normalize the values
+              normalized<-as.data.frame(t(normalize))
+            }  
+            
+            return(format(normalized,...))
+          })
